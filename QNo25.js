b4680ba2636dd14852d796f04323d22b1b91569b
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:47:02 +0500

    Add files via upload

---
+++ QNo25.js
@@ -0,0 +1,23 @@
+"use strict";
+// // 25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// // • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+Object.defineProperty(exports, "__esModule", { value: true });
+// // • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+let alien = ("Green");
+if (alien === "Blue") {
+    console.log("you earn 5 point");
+}
+else if (alien === "Green") {
+    console.log("you earn 5 point");
+}
+else if (alien === "Yellow") {
+    console.log("you earn 5 point");
+}
+// let alien : string=("green")
+// if(alien==="green"){
+//     console.log("players just earned 5 points");
+// }
+// alien = ("Yellow")
+// if(alien ==="green"){
+//     console.log("player just earned 5 point");
+// }
